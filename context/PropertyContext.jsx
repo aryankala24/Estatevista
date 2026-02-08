3dc24a9281aab00b420aedde5e48b915d4c2c551
aryankala <aryankala250@gmail.com>
2026-02-08 09:58:24 +0530

    Delete context directory

--- context/PropertyContext.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import data from "../data/properties.json";
-const C=createContext();
-export function useProperties(){return useContext(C);}
-export function PropertyProvider({children}){
- const [properties,setProperties]=useState([]);
- useEffect(()=>{setProperties(data);},[]);
- return(<C.Provider value={{properties,setProperties}}>{children}</C.Provider>);
-}
